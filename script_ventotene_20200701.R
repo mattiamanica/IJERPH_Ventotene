9a1b4d7fbd8a7a6fc2a18ed542abc03ad5fb0409
Mattia Manica <manica.mattia@gmail.com>
2020-07-02 14:39:49 +0200

    complete analysis and main figures

--- script_ventotene_20200701.R
+++ script_ventotene_20200701.R
@@ -4,6 +4,8 @@
 # load packages
 library(here)
 library(tidyverse)
+library(colorspace)
+library(purrr)
 
 # import data
 
@@ -132,12 +134,69 @@ for(i in 1:nrow(db)){
   db$build[i] <- buildings/area
 }
 
+db %>%group_by(IDsite,build)%>%count()%>%arrange(build)
 
 
+btrapnosea  <- gIntersection(ventotene2,btrap,byid=TRUE)
+
+par(mar=c(0,0,0,0))
+plot(vent,lwd=2)
+plot(utm.build,add=T)
+plot(utm.trap,add=T,col="red",pch=19,cex=1.5)
+plot(btrapnosea,add=T,lwd=2,fill="red")
+
+isl <- fortify(ventotene2)
+bld <- fortify(utm.build,by=ID)
+nos <- fortify(btrapnosea)
+trp <- as.data.frame(utm.trap@coords)
+
+f1a <-ggplot()+geom_path(data= isl,aes(long,lat))+
+  geom_polygon(data= nos,aes(long,lat,group=group),col="black",fill="grey90",alpha=0.05)+
+  geom_path(data= bld,aes(long,lat,group=group))+
+  geom_point(data = trp, aes(X1,X2),col="red",size=2)+
+  theme_bw() + theme(legend.position = "top",
+                     panel.grid.major.x = element_blank(),
+                     panel.grid.major.y = element_line(colour = "grey60",linetype = "dashed"),
+                     plot.title = element_text(size = rel(1.5), 
+                                               face = "bold", vjust = 1.5),
+                     axis.title = element_blank(),
+                     axis.text = element_blank(),
+                     strip.background =  element_rect(fill="white"),
+                     strip.text.x = element_text(size=15,face="italic"))+
+  coord_quickmap()
+f1a
+ggsave(f1a, filename = "fig1a.pdf",width = 5, height = 4,dpi=600)
 
 # exploratory analysis ###########################
 
 summary(db)
+
+
+# descriptive statistics #########################
+
+# samplings when
+range(unique(db$week))
+length(unique(db$week))
+
+# samplings where
+length(unique(db$SiteID))
+
+
+db %>% group_by(Species) %>% 
+  summarise(Tot    = sum(value),
+            Mean   = mean(value),
+            std    = sd(value),
+            Median = median(value),
+            quantL = quantile(value,0.25),
+            quantH = quantile(value,0.75))
+
+
+
+library(MASS)
+db %>% group_by(Species,IDsite,build) %>% 
+  summarise(Tot    = sum(value)) %>%
+  split(.$Species) %>% map(.,~glm.nb(Tot~build, data = .)) %>%
+  map(summary)
 
 # no missing values
 table(db$week,db$SiteID)
@@ -180,9 +239,27 @@ db %>% mutate(zero = case_when(value == 0~1,
 
 # overall mosquito dynamics
 
-db %>% group_by(Species,week) %>% summarise(Tot = sum(value)) %>%
+f1c <- db %>% group_by(Species,week) %>% 
+  summarise(Tot    = sum(value)) %>%
   ggplot(., aes(x=week,y=Tot,col=Species))+
-  geom_line()+  theme_bw()
+  geom_line()+ geom_point()+
+  geom_vline(xintercept = 32,linetype = "dashed")+
+  scale_color_manual(values = diverging_hcl(2)) +
+  theme_bw()  +
+  theme(legend.position = "top",
+        panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey60",linetype = "dashed"),
+        plot.title = element_text(size = rel(1.5), 
+                                  face = "bold", vjust = 1.5),
+        axis.title = element_text(face = "bold"),
+        axis.title.y = element_text(vjust= 1.8),
+        axis.title.x = element_text(vjust= -0.5),
+        strip.background =  element_rect(fill="white"),
+        strip.text.x = element_text(size=15,face="italic")
+  )
+f1c 
+
+ggsave(f1c, filename = "fig1c.pdf",width = 5, height = 4,dpi=600)
 
 # a disinfestation was reported on week 32, which is consistent with the observed pattern
 
@@ -214,12 +291,12 @@ db %>% filter(Species == "Culex") %>% group_by(IDsite,Long,Lat)%>%
 
 
 
-db %>% group_by(Species,IDsite) %>% summarise(Tot = sum(value))%>%
+f1b <- db %>% group_by(Species,IDsite) %>% summarise(Tot = sum(value))%>%
 ggplot(., aes(x=IDsite,y=Tot))+ theme_bw()+
   geom_bar(stat="identity",col="black",fill="grey60")+
   facet_grid( ~Species)+
-  labs(x = "ID Trap", 
-       y = "N? of trapped mosquito")  +
+  labs(x = "ID Site", 
+       y = "N° of trapped mosquito")  +
   theme_bw()  +
   theme(panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(colour = "grey60",linetype = "dashed"),
@@ -231,6 +308,9 @@ ggplot(., aes(x=IDsite,y=Tot))+ theme_bw()+
         strip.background =  element_rect(fill="white"),
         strip.text.x = element_text(size=15,face="italic")
   )
+f1b
+
+ggsave(f1b, filename = "fig1b.pdf",width = 10, height = 4.5,dpi=600)
 
 
 library(ggridges)
@@ -239,7 +319,7 @@ db %>% group_by(Species,IDsite,week) %>% summarise(Tot = sum(value))%>%
   geom_ridgeline(alpha=0.75)+
   facet_grid( ~Species)+
   labs(x = "ID Trap", 
-       y = "N? of trapped mosquito")  +
+       y = "N° of trapped mosquito")  +
   theme_bw()  +
   theme(panel.grid.major.x = element_blank(),
         panel.grid.major.y = element_line(colour = "grey60",linetype = "dashed"),
@@ -253,6 +333,24 @@ db %>% group_by(Species,IDsite,week) %>% summarise(Tot = sum(value))%>%
   )
 
 
+db$Species_site <- factor(paste(db$Species,db$IDSite,sep="_"))
+
+db %>% ggplot(.,aes(x=IDsite,y=value,fill=Species))+geom_boxplot()+
+  labs(x = "ID Site", 
+       y = "N° of trapped mosquito")  +
+  theme_bw()  +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey60",linetype = "dashed"),
+        plot.title = element_text(size = rel(1.5), 
+                                  face = "bold", vjust = 1.5),
+        axis.title = element_text(face = "bold"),
+        axis.title.y = element_text(vjust= 1.8),
+        axis.title.x = element_text(vjust= -0.5),
+        strip.background =  element_rect(fill="white"),
+        strip.text.x = element_text(size=15,face="italic")
+  )+
+  scale_fill_manual(values = diverge_hcl(2))+
+  scale_color_manual(values = diverge_hcl(2))
 
 
 
@@ -260,9 +358,126 @@ db %>% group_by(Species,IDsite,week) %>% summarise(Tot = sum(value))%>%
 
 
 
+#### model #######
+
+library(mgcv)
+# citation("mgcv")
+# Wood, S.N. (2017) 
+# Generalized Additive Models: An Introduction with R (2nd edition). 
+# Chapman and Hall/CRC.
+
+# poisson distribution
+
+m1 <- gam(value ~ Species+build+Species:build+
+            s(week,by=Species) , 
+          family = poisson, data = db)
+
+# overdispersion
+sum(resid(m1,type="pearson")^2)/(m1$df.residual)
+
+# negative binomial distribution
+library(MASS)
+m2 <- gam(value ~ Species+build+Species:build+
+            s(week,by=Species) , 
+          family = nb(), data = db)
+
+
+# overdispersion
+sum(resid(m2,type="pearson")^2)/(m2$df.residual) #ok
+
+# model validation / check assumptions
+gam.check(m2)
+qq.gam(m2)
+
+plot(resid(m2,type="pearson"))
+plot(fitted(m2),resid(m2,type="pearson"))
+plot(db$IDsite, resid(m2,type="pearson"))
+plot(db$Species, resid(m2,type="pearson"))
+plot(db$Lat, resid(m2,type="pearson"))
+plot(db$Long,resid(m2,type="pearson"))
+plot(factor(db$week),resid(m2,type="pearson"))
+plot(factor(db$week),db$value)
+# quite ok
+
+db$E <- resid(m2,type="pearson")
+ggplot(db, aes(week,E,col=Species))+
+  facet_wrap(~IDsite)+geom_point()+theme_bw()
+# temporal autocorrelation
+
+for(i in levels(db$Species_site)){
+  acf(db$E[db$Species_site==i] )}
+# quite ok
+
+# spatial autocorrelation
+library(gstat)
+mydb <- data.frame(e = db$E, lt = db$Lat,ln =db$Long)
+coordinates(mydb)<- c("ln","lt")
+vario <- variogram(object = e~1,
+                   data = mydb,
+                   cressie = TRUE)
+vario
+plot(vario,pch=19)
+# ok
+
+# not perfect but acceptable
+
+# model results
+summary(m2)
+
+
+par(mfrow=c(1,2))
+plot(m2)
+
+# plotting results
+
+newdb <- expand.grid(week = seq(min(db$week),max(db$week),by=1),
+                     Species = levels(db$Species),
+                     build = seq(0,0.2,by=0.01))
+
+dim(newdb) 
+
+newdb$fit <- predict(m2,newdata = newdb,type="response")
+newdb$hi  <- exp(predict(m2,newdata = newdb) + 1.96* predict(m2,newdata = newdb,se.fit = TRUE)$se.fit)
+newdb$lo  <- exp(predict(m2,newdata = newdb) - 1.96* predict(m2,newdata = newdb,se.fit = TRUE)$se.fit)
+
+range(db$week)
+
+f2a <- newdb %>% filter(week%in% c(35))%>%
+  ggplot(.,aes(100*build,fit,col=Species))+
+  geom_ribbon(aes(x=100*build,ymin= lo,ymax=hi,fill=Species),col="white",alpha=0.2)+
+  geom_line()+theme_bw()+
+  xlab("% Area covered by buildings")+
+  ylab("Mean mosquito abundance")+theme(legend.position = "top")+
+  scale_fill_manual(values = diverge_hcl(2))+
+  scale_color_manual(values = diverge_hcl(2))+
+  ylim(c(0,23))
 
 
 
+f2b <- newdb %>% filter(build == 0.1)%>%
+  ggplot(.,aes(week,fit,col=Species))+
+  geom_ribbon(aes(x=week,ymin= lo,ymax=hi,fill=Species),col="white",alpha=0.2)+
+  geom_line()+theme_bw()+
+  xlab("Week")+
+  ylab("Mean mosquito abundance")+theme(legend.position = "top")+
+  scale_fill_manual(values = diverge_hcl(2))+
+  scale_color_manual(values = diverge_hcl(2))+
+  ylim(c(0,23))
+
+
+
+library(gridExtra)
+f2 <- grid.arrange(f2a,f2b,ncol=2)
+ggsave(f2, filename = "fig2.pdf",width = 8, height = 4,dpi=600)
+
+
+
+# difference between temporal smoothers
+library(mgcViz)
+set.seed(20200702)
+m3 <- getViz(m2)
+plotDiff(s1 = sm(m3, 1), s2 = sm(m3, 2)) + l_ciPoly() + 
+  l_fitLine() + geom_hline(yintercept = 0, linetype = 2)
 
 
 
